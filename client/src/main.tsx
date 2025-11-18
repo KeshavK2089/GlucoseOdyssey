1de17b0dceefa3d2aeb5a0235f95aea1751973af
KeshavK2089 <keshavkotteswaran@gmail.com>
2025-11-17 21:01:10 -0500

    Fix formatting issues in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,9 @@ import { Router } from "wouter";
 import { useHashLocation } from "wouter/use-hash-location";
 import App from "./App";
 import "./index.css";
-
 createRoot(document.getElementById("root")!).render(
   <Router hook={useHashLocation}>
     <App />
   </Router>
 );
+
